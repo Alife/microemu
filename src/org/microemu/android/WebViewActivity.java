25907118f1397c7e2ec91d0c4a21ad7bcf0d15d3
lukey <lukey82+git.opm@gmail.com>
2014-03-12 12:45:01 -0400

    add WebViewActivity.java

--- src/org/microemu/android/WebViewActivity.java
+++ src/org/microemu/android/WebViewActivity.java
@@ -1,77 +1,73 @@
 package org.microemu.android;
 
-import com.opera.mini.mod422.R;
-
+import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceScreen;
-import android.view.View;
-import android.view.WindowManager;
-import android.view.View.OnClickListener;
-import android.view.WindowManager.LayoutParams;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
+import android.widget.Toast;
 
-public class WebViewActivity extends PreferenceActivity {
-	RelativeLayout panel;
-	SharedPreferences prefs;
-	PreferenceScreen preferenceScreen;
-
-	public void onCreate(Bundle paramBundle) {
-		super.onCreate(paramBundle);
+import com.opera.mini.mod422.R;
+ 
+public class WebViewActivity extends Activity{
+	private WebView webView;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.webview);
+		webView = (WebView) findViewById(R.id.webView1);
+		
+		webView.getSettings().setJavaScriptEnabled(true);//设置使用够执行JS脚本
+		webView.getSettings().setBuiltInZoomControls(true);//设置使支持缩放
+		//webView.getSettings().setDefaultFontSize(5);
+			
+		String url = "http://google.com";
 		if (Intent.ACTION_VIEW.equals(getIntent().getAction())) {
-			String dataString = getIntent().getDataString();
-	
-			final LayoutParams params = new WindowManager.LayoutParams();
-			final RelativeLayout panel = new RelativeLayout(this);
-			setContentView(panel);
-			RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
-					LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
-			panel.setLayoutParams(lp);
-			LinearLayout localLinearLayout = new LinearLayout(this);
-			panel.addView(localLinearLayout);
-			ImageView close = new ImageView(this);
-			close.setImageResource(R.drawable.close);
-			close.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					finish();
-				}
-			});
-			
-			final WebView webView = new WebView(this);
-			webView.getSettings().setJavaScriptEnabled(false);// 设置使用够执行JS脚本
-			webView.getSettings().setBuiltInZoomControls(true);// 设置使支持缩放
-			// webView.getSettings().setDefaultFontSize(5);
-			webView.loadUrl(dataString);
-			//此方法可以处理webview 在加载时和加载完成时一些操作
-			webView.setWebViewClient(new WebViewClient(){
-			//重写此方法表明点击网页里面的链接还是在当前的webview里跳转，不跳到浏览器那边
-				@Override
-					public boolean shouldOverrideUrlLoading(WebView view, String url) {  
-						view.loadUrl(url);
-					return true;
-				}
-			});
-			localLinearLayout.addView(webView);
-			
-			lp = new RelativeLayout.LayoutParams(
-					LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
-			lp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);//与父容器的左侧对齐
-			lp.addRule(RelativeLayout.ALIGN_PARENT_TOP);//与父容器的上侧对齐
-//			lp.leftMargin=30;
-//			lp.topMargin=30;
-			close.setId(1);//设置这个View 的id 
-			close.setLayoutParams(lp);//设置布局参数
-	
-			webView.addView(close);
+			url = getIntent().getDataString();
+		}
+		webView.loadUrl(url);
+		webView.setWebChromeClient(new chromeClient()); 
+		webView.setWebViewClient(new WebViewClient(){
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				// TODO Auto-generated method stub
+				view.loadUrl(url);// 使用当前WebView处理跳转
+				return true;//true表示此事件在此处被处理，不需要再广播
+			}
+			@Override	//转向错误时的处理
+			public void onReceivedError(WebView view, int errorCode,
+					String description, String failingUrl) {
+				// TODO Auto-generated method stub
+				Toast.makeText(WebViewActivity.this, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+	@Override	//默认点回退键，会退出Activity，需监听按键操作，使回退在WebView内发生
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+		if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+			webView.goBack();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	class chromeClient extends WebChromeClient {
+		@Override
+		public void onProgressChanged(WebView view, int newProgress) {
+			// 动态在标题栏显示进度条
+			WebViewActivity.this.setProgress(newProgress * 100);
+			super.onProgressChanged(view, newProgress);
 		}
 
+		@Override
+		public void onReceivedTitle(WebView view, String title) {
+			// 设置当前activity的标题栏
+			WebViewActivity.this.setTitle(title);
+			super.onReceivedTitle(view, title);
+		}
 	}
-	
-}
+
+ } 
